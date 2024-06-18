7ac4735697be0c8cd752665dc820e7ed6b20ad64
tirth41 <tirthspatel1116@gmail.com>
2024-06-17 22:44:18 -0400

    Register page

--- app/auth/register.tsx
+++ app/auth/register.tsx
@@ -1,10 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import { useRouter } from 'expo-router';
+import { API } from '@/constants/Utility';
 
 export default function register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const router = useRouter();
+
+
+
+  const handleRegisterPage = async () =>{
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        confirmPassword: confirmPassword,
+      }),
+    };
+    try {
+      console.log(API);
+        const response = await fetch(API + '/auth/register',requestOptions);
+      
+      if(response.ok){
+        
+        router.push("(tabs)");
+      }else{
+        Alert.alert('Error!!', 'Username already exist', [
+          {
+            text: 'Cancel',
+            
+          },
+          {text: 'OK'},
+        ]);
+    
+          console.log("Login Failed with status: " + await response.status);
+      }
+    } catch (error) {
+        console.log(error);
+    }
+   };
+    
+   
+
+  
+  const confirmpassword = () =>{
+
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>register</Text>
+      
+        <TextInput
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Confirm Password"
+          value ={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+          style={styles.input}
+        />
+        <Button title="Register" onPress={handleRegisterPage} />
     </View>
   )
 }
@@ -15,5 +88,13 @@ const styles = StyleSheet.create({
         backgroundColor:'white',
         justifyContent: 'center',
         alignItems: 'center',
+      },
+      input: {
+        
+        width: '90%',
+        margin: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 5,
       },
 })
